b6cc25fa45fa43b3ecf564878dbc739f3450f003
Vanegas <juegosforpc@gmail.com>
2018-10-11 16:14:14 -0500

    trabajo_hecho_en_clase
    Este código fue lo que los compañeros nos enseñaron a realizar con Angular

---
+++ dojo/src/app/moduls/employee.ts
@@ -0,0 +1,16 @@
+export class Employee {
+    constructor(
+        _id: '',
+        nombre: '',
+        position: '',
+        office: '',
+        salary: 0,
+
+    ){}
+    _id: String;
+    nombre: String;
+    position: String;
+    office: String;
+    salary: number;
+
+}
